bd47a1ba0a900f2249159e588fda075133d5f339
RebuyBoy <67625553+RebuyBoy@users.noreply.github.com>
2023-05-07 21:16:48 +0700

    init

---
+++ dispatcher/src/main/java/org/reb/controller/TelegramBot.java
@@ -0,0 +1,43 @@
+package org.reb.controller;
+
+import lombok.extern.log4j.Log4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+@Log4j
+public class TelegramBot extends TelegramLongPollingBot {
+
+    @Value("${telegram.bot.username}")
+    private String botUsername;
+    @Value("${telegram.bot.token}")
+    private String botToken;
+
+    @Override
+    public void onUpdateReceived(Update update) {
+       log.debug(update.getMessage().getText());
+        var response = SendMessage.builder()
+            .chatId(update.getMessage().getChatId())
+            .text("Hello")
+            .build();
+        try {
+            execute(response);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botUsername;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+}
